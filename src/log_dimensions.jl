54cd36344de1385c1487def7aee6b93e11ea47f9
icweaver <weaveric@gmail.com>
2025-08-03 14:59:30 -0700

    starting to add more patches and tests

--- src/log_dimensions.jl
+++ src/log_dimensions.jl
@@ -1,10 +1,10 @@
-using TestItems: @testitem
+using TestItems: @testitem, @testmodule
 
-abstract type LogUnit end
+abstract type LogUnit{T} end
 
-struct MagUnit <: LogUnit
-    value::Float64
-    zero_point::Quantity
+struct MagUnit{T<:Real} <: LogUnit{T}
+    value::T
+    zero_point::Quantity{Float64, Dimensions{DEFAULT_DIM_BASE_TYPE}}
     name::Symbol
 end
 
@@ -22,17 +22,23 @@ function uconvert(mout::MagUnit, x)
     @assert mout.value == 1.0 "You tried converting to $(mout), a `MagUnit` with a non-unit value."
     return -2.5 * log10(uexpand(x) / mout.zero_point) * mout
 end
-uconvert(qout::UnionAbstractQuantity{<:Any, <:Dimensions}, m::MagUnit) = uconvert(qout, uexpand(m))
+uconvert(qout::UnionAbstractQuantity{<:Any, <:SymbolicDimensions}, m::MagUnit) = uconvert(qout, uexpand(m))
 uconvert(mout::MagUnit, m::MagUnit) = uconvert(mout, uexpand(m))
 
 # Convenience
 Base.:(|>)(m, mout::MagUnit) = uconvert(mout, m)
-Base.:(|>)(m::MagUnit, mout) = uconvert(mout, m)
+Base.:(|>)(m::UnionAbstractQuantity{<:Any, <:SymbolicDimensions}, mout::MagUnit) = uconvert(mout, m)
+Base.:(|>)(m::MagUnit, mout::UnionAbstractQuantity{<:Any, <:SymbolicDimensions}) = uconvert(mout, m)
 Base.:(|>)(m::MagUnit, mout::MagUnit) = uconvert(mout, m)
 
-# Basic patches
-Base.one(m::MagUnit) = one(m.value)
+# Some basic patches
+Base.one(unit::LogUnit) = one(m.value)
 Base.oneunit(m::MagUnit) = MagUnit(one(m.value), m.zero_point, m.name)
+ustrip(unit::LogUnit) = unit.value
+
+Base.isapprox(m1::MagUnit, m2::MagUnit; atol=(zero ∘ ustrip)(m1.value), kwargs...) = isapprox(ustrip(m1), ustrip(m2); atol, kwargs...)
+Base.isapprox(m1::MagUnit, q2::UnionAbstractQuantity; atol=zero(q2), kwargs...) = isapprox(uexpand(m1), uexpand(q2); atol=uexpand(atol), kwargs...)
+Base.isapprox(q1::UnionAbstractQuantity, m2::MagUnit; kwargs...) = isapprox(m2, q1; kwargs...)
 
 Base.:*(value::Number, m::MagUnit) = MagUnit(value, m.zero_point, m.name)
 
@@ -76,8 +82,17 @@ macro ul_str(s)
 end
 
 # Tests
-@testitem "Zero point" begin
+@testmodule DQ begin
     using DynamicQuantities
 
-    @test isapprox(2u"cm", 2u"cm")
+    const u = DynamicQuantities.Units
+    const ul = DynamicQuantities.LogUnits
+end
+
+@testitem "Zero-point" setup=[DQ] begin
+
+    @test isapprox(1DQ.ul.V_mag, 3640DQ.u.Jy; atol=0.001DQ.ul.V_mag)
+end
+
+@testitem "Algebraic operations" begin
 end
